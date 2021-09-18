5155924363a0de921ddfd3be619a4bb144d3abe4
Federico Musarra <fede.musarra@hotmail.com>
2021-09-18 18:17:41 -0300

    desafio1

---
+++ Clase1Musarra/main.js
@@ -0,0 +1,7 @@
+let saludo = prompt("Hola como estas? ingresa tu nombre")
+let numero1 =Number (parseInt(prompt ("ingresa un numero")))
+let numero2 =Number(parseInt(prompt ("ingresa un numero")))
+let numero3 = numero1 + numero2
+
+
+alert(saludo + " " + "el resultado de la encuesta es" + " " + numero3)
