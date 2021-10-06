0824b3db6b32439583b0e2f6dbd7ee1718dab912
Federico Musarra <fede.musarra@hotmail.com>
2021-10-06 16:00:44 -0300

    Desafio4

---
+++ Desafio Clase 4/main.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 5; i++){ 
+   // En cada repetición solicitamos un nombre.
+   let ingresarNombre = prompt("Ingresar nombre");
+   // Informamos el turno asignado usando el número de repetición (i).
+   alert(" Turno  N° "+i+" Nombre: "+ingresarNombre);
+   
+}
